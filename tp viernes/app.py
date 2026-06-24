7e7f20c7cbb2e4cb62bc7154a0811c1947af0a31
julio15456326 <julio15456326@gmail.com>
2026-06-24 12:10:58 -0300

    correccion de nombre de imagenes

--- tp viernes/app.py
+++ tp viernes/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, session, redirect, flash
+juegocfrom flask import Flask, render_template, request, url_for, session, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func 
 import os
